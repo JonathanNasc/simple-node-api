b0afd38b12c469ba57f0672d18f940ee8c34eee3
jnasc <jonathan.nasc@yahoo.com.br>
2015-11-17 22:48:02 -0200

    Add first files

---
+++ server.js
@@ -0,0 +1,31 @@
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8080;
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES FOR API
+// =============================================================================
+var router = express.Router();
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+    console.log("OK");
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
